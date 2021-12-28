9dc60db438b51f006c154ba1aef455f2e9f97cab
mkrkru <mkrkru@makarkas-macbook.local>
2021-12-28 18:51:33 +0300

    f

--- serve.js
+++ serve.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
 
+const app = (request, response) => require('serve-handler')(request, response, { public: "build" });
+
 require('https')
     .createServer({
-        key: fs.readFileSync('certs/private.key'),
-        cert: fs.readFileSync('certs/certificate.crt')
-    }, (request, response) =>
-        require('serve-handler')(request, response, { public: "build" }))
-    .listen(443, () =>
-        console.log('Running at http://localhost:443'));
+        key: fs.readFileSync('./certs/private.key'),
+        cert: fs.readFileSync('./certs/certificate.crt')
+    }, app)
+    .listen(443, () => console.log('Running at https://localhost'));
+
+require('http')
+    .createServer(app)
+    .listen(80, () => console.log('Running at http://localhost'));
