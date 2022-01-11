8d354cb7b9e62cfffed806dd245b6669ba2b262f
Makarka <9348849@mail.ru>
2022-01-12 00:48:49 +0300

    g

--- src/apps/mint/onMint.tsx
+++ src/apps/mint/onMint.tsx
@@ -1,11 +1,12 @@
 // import pinataSDK from '@pinata/sdk';
 // const pinata = pinataSDK('08d0ea34d82743359669', '29cac61ba50e631b1dc6ae40fc64a294417d8e84732a8083b93b70a0fe63cdca');
+
 import contractABI from "./contract-abi.json";
 import { createAlchemyWeb3 } from "@alch/alchemy-web3";
 
 const Alchemy = createAlchemyWeb3(process.env.REACT_APP_ALCHEMY_KEY);
 const rand = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
-const resMeta = `https://gateway.pinata.cloud/ipfs/QmPM3BrVj5A7dgq8oeX2MdZ7YxmvXvakC9WMpLaWAbvuAr/nft${rand(0, 100)}.json`;
+const resMeta = `ipfs://QmPM3BrVj5A7dgq8oeX2MdZ7YxmvXvakC9WMpLaWAbvuAr/nft${rand(0, 100)}.json`;
 
 export default async function onMint(account: string) {
     const Contract = await new Alchemy.eth.Contract(contractABI, process.env.REACT_APP_CONTRACT_ADDRESS);
@@ -13,13 +14,13 @@ export default async function onMint(account: string) {
         from: account,
         to: process.env.REACT_APP_CONTRACT_ADDRESS,
         gas: "30000",
-        maxFeePerGas: "1000000108",
-        value: `${0.035 * 1000000000000000000}`,
+        //value: `${0.035 * 1000000000000000000}`,
         data: Contract.methods.mintNFT(account, resMeta).encodeABI()
     };
 
     Alchemy.eth.sendTransaction(tx).then((rx: any) => {
-        console.log(`https://rinkeby.etherscan.io/tx/${rx}`);
+        console.log(rx);
+        //console.log(`https://rinkeby.etherscan.io/tx/${rx}`);
         console.log(resMeta);
     });
 
